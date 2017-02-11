581948bdbb5106ad60872801b5dac2af66f6ed17
Zhao Zhongming_CN04083 <ZHAOZ1@airproducts.com>
2017-02-11 08:44:52 +0800

    create test data from prod copy

--- DeviceSimulate/Common/Message.cs
+++ DeviceSimulate/Common/Message.cs
@@ -1,6 +1,7 @@
 ﻿using Newtonsoft.Json;
 using System;
 using System.Collections.Generic;
+using System.Diagnostics;
 using System.Linq;
 using System.Runtime.Serialization;
 using System.Text;
@@ -8,30 +9,166 @@ using System.Threading.Tasks;
 
 namespace Common
 {
-    public class Message
-    {
-        [JsonProperty(PropertyName = @"Location ID")]
-        public int LocationID { get; set; }
-        [JsonProperty(PropertyName = @"Node number")]
-        public int NodeNumber { get; set; }
-        public float Ch0 { get; set; }
-        public float Ch1 { get; set; }
-        public float Ch2 { get; set; }
-        public float Ch3 { get; set; }
-        public float Ch4 { get; set; }
-        public float Ch5 { get; set; }
-        public float Ch6 { get; set; }
-        public float Ch7 { get; set; }
-        public float Ch8 { get; set; }
-        public float Ch9 { get; set; }
-        public float Ch10 { get; set; }
-        public float Ch11 { get; set; }
-        public float Ch12 { get; set; }
-        public float Ch13 { get; set; }
-        public float Ch14 { get; set; }
-        public float Ch15 { get; set; }
-        [JsonProperty(PropertyName = @"UTC Timestamp")]
-        public DateTime UTCTimestamp { get; set; }
 
+    [
+    DebuggerDisplay("{BpcUid}:{WinUid}-{TimeStamp}"),
+    DataContract
+    ]
+    public class InputDTO
+    //: IEnumerable<float>
+    {
+        public InputDTO()
+        {
+            this.__values = new float[16];
+        }
+
+        /// <summary>Board PC unique identification number.</summary>
+        ///
+        [
+        DataMember(Order = 0, Name = @"bpc")
+        ]
+        public int BpcUid
+        {
+            get { return this._BpcUid; }
+            set { _BpcUid = value; }
+        }
+        private int _BpcUid;
+
+        /// <summary>WIN unique identification number.</summary>
+        ///
+        [
+        DataMember(Order = 1, Name = @"win")
+        ]
+        public int WinUid { get; set; }
+
+        /// <summary>WIN unique identification number.</summary>
+        ///
+        [
+        DataMember(Order = 2, Name = @"time")
+        ]
+        public DateTime TimeStamp { get; set; }
+
+        /// <summary>Input value from channel #01.</summary>
+        ///
+        [IgnoreDataMember]
+        public float Ch01 { get { return this._values[0]; } set { this._values[0] = value; } }
+
+        /// <summary>Input value from channel #02.</summary>
+        ///
+        [IgnoreDataMember]
+        public float Ch02 { get { return this._values[1]; } set { this._values[1] = value; } }
+
+        /// <summary>Input value from channel #03.</summary>
+        ///
+        [IgnoreDataMember]
+        public float Ch03 { get { return this._values[2]; } set { this._values[2] = value; } }
+
+        /// <summary>Input value from channel #04.</summary>
+        ///
+        [IgnoreDataMember]
+        public float Ch04 { get { return this._values[3]; } set { this._values[3] = value; } }
+
+        /// <summary>Input value from channel #05.</summary>
+        ///
+        [IgnoreDataMember]
+        public float Ch05 { get { return this._values[4]; } set { this._values[4] = value; } }
+
+        /// <summary>Input value from channel #06.</summary>
+        ///
+        [IgnoreDataMember]
+        public float Ch06 { get { return this._values[5]; } set { this._values[5] = value; } }
+
+        /// <summary>Input value from channel #07.</summary>
+        ///
+        [IgnoreDataMember]
+        public float Ch07 { get { return this._values[6]; } set { this._values[6] = value; } }
+
+        /// <summary>Input value from channel #08.</summary>
+        ///
+        [IgnoreDataMember]
+        public float Ch08 { get { return this._values[7]; } set { this._values[7] = value; } }
+
+        /// <summary>Input value from channel #09.</summary>
+        ///
+        [IgnoreDataMember]
+        public float Ch09 { get { return this._values[8]; } set { this._values[8] = value; } }
+
+        /// <summary>Input value from channel #10.</summary>
+        ///
+        [IgnoreDataMember]
+        public float Ch10 { get { return this._values[9]; } set { this._values[9] = value; } }
+
+        /// <summary>Input value from channel #11.</summary>
+        ///
+        [IgnoreDataMember]
+        public float Ch11 { get { return this._values[10]; } set { this._values[10] = value; } }
+
+        /// <summary>Input value from channel #12.</summary>
+        ///
+        [IgnoreDataMember]
+        public float Ch12 { get { return this._values[11]; } set { this._values[11] = value; } }
+
+        /// <summary>Input value from channel #13.</summary>
+        ///
+        [IgnoreDataMember]
+        public float Ch13 { get { return this._values[12]; } set { this._values[12] = value; } }
+
+        /// <summary>Input value from channel #14.</summary>
+        ///
+        [IgnoreDataMember]
+        public float Ch14 { get { return this._values[13]; } set { this._values[13] = value; } }
+
+        /// <summary>Input value from channel #15.</summary>
+        ///
+        [IgnoreDataMember]
+        public float Ch15 { get { return this._values[14]; } set { this._values[14] = value; } }
+
+        /// <summary>Input value from channel #16.</summary>
+        ///
+        [IgnoreDataMember]
+        public float Ch16 { get { return this._values[15]; } set { this._values[15] = value; } }
+
+        [IgnoreDataMember]
+        private const int _CountValue = 16;
+
+        public int Count { get { return _CountValue; } }
+
+        public float this[int index]
+        {
+            get { return this._values[index]; }
+            set { this._values[index] = value; }
+        }
+
+        private float[] __values;
+
+        [
+        DataMember(Order = 20, Name = @"data"),
+        DebuggerBrowsable(DebuggerBrowsableState.Never)
+        ]
+        private float[] _values
+        {
+            get
+            {
+                if (__values == null)
+                    __values = new float[16];
+                return __values;
+            }
+            set
+            {
+                __values = value;
+            }
+        }
+
+
+        //public IEnumerator<float> GetEnumerator()
+        //{
+        //    foreach (var v in this._values)
+        //        yield return v;
+        //}
+
+        //System.Collections.IEnumerator System.Collections.IEnumerable.GetEnumerator()
+        //{
+        //    return this.GetEnumerator();
+        //}
     }
 }
--- DeviceSimulate/DeviceSimulate/Program.cs
+++ DeviceSimulate/DeviceSimulate/Program.cs
@@ -6,6 +6,7 @@ using System.Threading.Tasks;
 using Microsoft.Azure.Devices.Client;
 using Newtonsoft.Json;
 using log4net;
+using Common;
 
 namespace DeciveSimulate
 {
@@ -29,9 +30,24 @@ namespace DeciveSimulate
 
         #region dev
         static string iotHubUri = "ap-Iot-APGateway-USE-Dev.azure-devices.net";
-        static string deviceKey = "X9XI62RaRjSoVghhQkRalHIqkzwoNTwLpShtWX6XFR4=";
-        static string deviceIdDefined = "zhaoz1";
+        //static string deviceKey = "X9XI62RaRjSoVghhQkRalHIqkzwoNTwLpShtWX6XFR4=";
+        //static string deviceIdDefined = "zhaoz1";
+
+        static string deviceKey = "uZv0UuPO3N4capCOUOTfBEmULnWMK/n97I1yGXYgKeA=";
+        static string deviceIdDefined = "marvin1";
         #endregion
+
+        private static InputDTO[] testDataDic
+         = new InputDTO[] { new InputDTO { BpcUid = 50, WinUid = 12 }
+                            , new InputDTO { BpcUid = 50, WinUid = 55}
+                            , new InputDTO { BpcUid = 81, WinUid = 10}
+                            , new InputDTO { BpcUid = 78, WinUid = 1}
+                            , new InputDTO { BpcUid = 78, WinUid = 2}
+                            , new InputDTO { BpcUid = 63, WinUid = 2}
+                            , new InputDTO { BpcUid = 179, WinUid = 1}
+                            , new InputDTO { BpcUid = 212, WinUid = 2}
+                            , new InputDTO { BpcUid = 50, WinUid = 6}
+                            , new InputDTO { BpcUid = 50, WinUid = 9} };
 
 
         static ILog log = null;
@@ -46,33 +62,48 @@ namespace DeciveSimulate
 
             ReceiveC2dAsync();
 
-            int win = 101;
-            int bpc = 121;
-
             while (true)
             {
-                if (win > 104) win = 101;
-                SendDeviceToCloudMessagesAsync(bpc, win++);
+                SendDeviceToCloudMessagesAsync(GenerateRandomData());
                 Task.Delay(1000).Wait();
-                
+
                 Console.ReadLine();
             }
         }
 
-        private static async void SendDeviceToCloudMessagesAsync(int bpc, int win)
+        private static InputDTO GenerateRandomData()
         {
-            float avgWindSpeed = 10; // m/s
             Random rand = new Random();
-            float currentWindSpeed = Convert.ToSingle(avgWindSpeed + rand.NextDouble() * 4 - 2);
 
-            var telemetryDataPoint = new
+            InputDTO inputDTO = new InputDTO
             {
-                bpc = bpc,
-                win = win,
-                time = DateTime.Now.ToUniversalTime(),
-                data = new Single[] { currentWindSpeed, 1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 11, 12, 13, 14, 15 }
+                BpcUid = testDataDic[rand.Next(0,9)].BpcUid,
+                WinUid = testDataDic[rand.Next(0, 9)].WinUid,
+                TimeStamp = DateTime.Now.ToUniversalTime(),
+                Ch01 = Convert.ToSingle(rand.NextDouble() * 4),
+                Ch02 = Convert.ToSingle(rand.NextDouble() * 4),
+                Ch03 = Convert.ToSingle(rand.NextDouble() * 2),
+                Ch04 = Convert.ToSingle(rand.NextDouble() * 0.1),
+                Ch05 = Convert.ToSingle(rand.NextDouble() * 4),
+                Ch06 = Convert.ToSingle(rand.NextDouble() * 5),
+                Ch07 = Convert.ToSingle(rand.NextDouble() * 20),
+                Ch08 = Convert.ToSingle(rand.NextDouble() * 4),
+                Ch09 = Convert.ToSingle(rand.NextDouble() * 0.4),
+                Ch10 = Convert.ToSingle(rand.NextDouble() * 40),
+                Ch11 = Convert.ToSingle(rand.NextDouble() * 4),
+                Ch12 = Convert.ToSingle(rand.NextDouble() * 4),
+                Ch13 = Convert.ToSingle(rand.NextDouble()),
+                Ch14 = Convert.ToSingle(rand.NextDouble() * 4),
+                Ch15 = Convert.ToSingle(rand.NextDouble() * 4),
+                Ch16 = Convert.ToSingle(rand.NextDouble())
             };
 
+            return inputDTO;
+        }
+
+     
+    private static async void SendDeviceToCloudMessagesAsync(InputDTO inputDTO)
+        {
             //var message = new Common.Message();
             //message.LocationID = bpc;
             //message.NodeNumber = win;
@@ -100,7 +131,7 @@ namespace DeciveSimulate
 
             //var messageString = "{'bpc':" + bpc + ",'win':" + win + ",'time':" + DateTime.Now.ToUniversalTime() + ",'data':[6435,3310,56.06875,60.325,3300,6288,-12161,6204,3293,55.01875,59.2125,3300,6257,1805,106,1901]}";
 
-            var messageString = JsonConvert.SerializeObject(telemetryDataPoint);
+            var messageString = JsonConvert.SerializeObject(inputDTO);
             var messageSend = new Message(Encoding.ASCII.GetBytes(messageString));
 
             await deviceClient.SendEventAsync(messageSend);
